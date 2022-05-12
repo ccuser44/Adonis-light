02b5ac535abfa6beffe1d8f83f93fca124eb2a6f
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2022-05-12 23:40:56 +0300

    Delete Quacker.server.lua

--- MainModule/Server/Dependencies/Assets/Quacker.server.lua
+++
@@ -1,9 +0,0 @@
-while true do
-	wait(math.random(5, 20))
-	if script.Parent ~= nil then
-		local name = "Quack" .. math.random(1, 4)
-		script.Parent:FindFirstChild(name):Play()
-	else
-		break
-	end
-end

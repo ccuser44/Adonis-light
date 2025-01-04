b1bc4eeab0272c4bcdf1974dea9448e74a577ff3
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2025-01-04 19:16:50 +0000

    Fix issues with unix build

--- build.sh
+++ build.sh
@@ -1,7 +1,25 @@
-#!/bin/bash
+#!/bin/sh
+
+printf "Updating Roblox standard library"
+selene generate-roblox-std
 
 printf "Checking for lint errors from ./Loader and ./MainModule"
 selene ./Loader ./MainModule
 
-printf "Running rojo build -o Adonis.rbxm"
-rojo build -o Adonis.rbxm
+printf "Building Adonis place file"
+rojo build -o Adonis.rbxl
+
+printf "Building Adonis Standalone"
+run: rojo build -o Adonis_Model.rbxm .github/build.project.json
+
+printf "Set DebugMode off"
+sed -i "s/DebugMode = true/DebugMode = false/g" Loader/Loader/Loader.server.luau
+
+printf "Set NightlyMode off"
+sed -i "s/NightlyMode = true/NightlyMode = false/g" Loader/Loader/Loader.server.luau
+
+printf "Building Adonis Loader"
+rojo build -o Adonis_Loader.rbxm .github/loader.deploy.project.json
+
+printf "Building Adonis MainModule"
+rojo build -o Adonis_MainModule.rbxm .github/module.deploy.project.json

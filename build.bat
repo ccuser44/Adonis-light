49fb7c66b19f8266f8482928d38fefcfcc3855c0
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2025-01-05 17:09:14 +0000

    Fix issues with windows build

--- build.bat
+++ build.bat
@@ -15,10 +15,32 @@ IF %ERRORLEVEL% NEQ 0 (
 	SET ROJO_COMMAND=rojo
 )
 
+echo Updating Roblox standard library
+selene generate-roblox-std
+
 echo Checking for lint errors with %SELENE_COMMAND% from ./Loader and ./MainModule 
 %SELENE_COMMAND% ./MainModule ./Loader
 
-echo Running %ROJO_COMMAND% build -o Adonis.rbxm
-%ROJO_COMMAND% build -o Adonis.rbxm
+echo Using %ROJO_COMMAND% for rojo
+
+echo Building Adonis place file
+rojo build -o Adonis.rbxl
+
+echo Building Adonis Standalone
+rojo build -o Adonis_Model.rbxm .github/build.project.json
+
+:: TODO: Make this use raw bat instead of powershell
+echo Set DebugMode off
+powershell -Command "(gc Loader/Loader/Loader.server.luau) -replace 'DebugMode = true', 'DebugMode = false' | Out-File -encoding ASCII Loader/Loader/Loader.server.luau"
+
+:: TODO: Make this use raw bat instead of powershell
+echo Set NightlyMode off
+powershell -Command "(gc Loader/Loader/Loader.server.luau) -replace 'NightlyMode = true', 'NightlyMode = false' | Out-File -encoding ASCII Loader/Loader/Loader.server.luau"
+
+echo Building Adonis Loader
+rojo build -o Adonis_Loader.rbxm .github/loader.deploy.project.json
+
+echo Building Adonis MainModule
+rojo build -o Adonis_MainModule.rbxm .github/module.deploy.project.json
 
 ENDLOCAL
